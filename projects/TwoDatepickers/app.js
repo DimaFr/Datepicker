4a36a519a866127fd2746c762f4a4b62bcb30171
dimitry.friedman <dima.friedman@gmail.com>
2014-09-09 15:05:57 +0300

    initial html started two datepickers implemented - should read data

--- projects/TwoDatepickers/app.js
+++ projects/TwoDatepickers/app.js
@@ -2,39 +2,40 @@
  * Created by dimitry.friedman on 8/26/2014.
  */
 (function (window, angular) {
-    angular.module('app',['ui.bootstrap']);
+    angular.module('app', ['ui.bootstrap']);
     angular.module('app')
-        .controller({'DatePickerController':['$scope',datePickerController]});
+        .controller({'DatePickerController': ['$scope', datePickerController]});
 
 
-   function datePickerController (scope){
+    function datePickerController(scope) {
+        this.sendNewsletter = false;
+        this.startDate = new Date();
+        this.minDate = this.startDate;
+        //TODO: change - start date plus 7 days
+        this.endDate = new Date(this.startDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+        this.dateOptions = {
+            showWeeks: 'false',
+            maxMode: 'day'
+        }
+        //start date picker
+        this.open1 = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            this.opened2 = false;
+            this.opened1 = true;
+        }
+        //end date picker
+        this.open2 = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            this.opened1 = false;
+            this.opened2 = true;
+        }
+        this.sendBtnClicked = function () {
+            console.log(this.startDate + "\n" + this.endDate + "\n" + this.email + "\n" + this.sendNewsletter)
+        }
 
-       this.startDate = new Date();
-       this.minDate = this.startDate;
-       //TODO: change - start date plus 7 days
-       this.endDate = new Date(this.startDate.getTime() + 7 * 24 * 60 * 60 * 1000);
-       this.dateOptions = {
-           showWeeks:'false',
-           maxMode:'day'
-       }
-      //start date picker
-       this.open1 = function($event){
-           $event.preventDefault();
-           $event.stopPropagation();
-           this.opened2=false;
-           this.opened1=true;
-       }
-       //end date picker
-       this.open2 = function($event){
-           $event.preventDefault();
-           $event.stopPropagation();
-           this.opened1=false;
-           this.opened2=true;
-       }
 
-
-
-
-   }
+    }
 
 })(window, angular);
